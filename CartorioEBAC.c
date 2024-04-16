0b3c82071cf602eaed58f73f201de0edac03171f
Magialis <167231725+Magialis@users.noreply.github.com>
2024-04-16 14:58:37 -0300

    Login
    Adição da tela de login para acessar ao menu

--- CartorioEBAC.c
+++ CartorioEBAC.c
@@ -137,56 +137,72 @@ int main()
 	int opcao=0; //definindo variáveis para a escolha do menu
 	//a variável aloca um espaço de memória, então um pedacinho da memória do computador se chama "opcao" e ela começa do 0 se não pode ficar um valor variável de memória. para avisar ao computador que aquela variável não está livre 
 	int laco=1; //definindo variáveis para voltar para o menu, repetição
-	
 	//for (laco=1 ; laco=10 ; laco++) -> "for" faz com que faça/repita todas vezes que eu pedir
 	//laco=1 a entrada quando for 1; laco=10 a saída quando for 10; laco++ toda vez que o programa entrar no laço vai aumentar em 1 o valor da variável
 	//mas para não encerrar o programa, deixamos que comece em laco=1 e continue em laco=1, sem incremento
-	for(laco=1;laco=1;)
+	char senhadigitada[10]="a";
+	int comparacao;
+	
+	setlocale(LC_ALL, "Portuguese");
+	printf("Cartório da EBAC \n\n");
+	printf("Login de administrador \n\nDigite a sua senha: ");
+	scanf("%s",senhadigitada);
+	
+	comparacao = strcmp(senhadigitada, "admin");
+	
+	if(comparacao == 0) //como o valor é igual ele salva igual a 0
 	{
-	
-		system("cls"); //comando para limpar a tela
+		system("cls");
 		
-		setlocale(LC_ALL, "Portuguese"); //definir o local para conseguir usar os acentos
-
-		printf("Cartório da EBAC \n\n"); //início do menu
-		printf("Digite a opção desejada do menu: \n\n");
-		printf("\t 1 - Registrar nomes \n");
-		printf("\t 2 - Consultar nomes \n");
-		printf("\t 3 - Deletar nomes \n"); 
-		printf("\t 4 - Sair do sistema \n\n");
-		printf("Opção: ");//fim do menu
-		//\n próxima linha. \t espaçamento, margem  
-	
-		scanf ("%d", &opcao); //armazenando as informações, a escolha do usuário
-		//scanf tem como objetivo scanear o que o usuário coloca. %d armazena uma variável do tipo inteiro. indicar em qual variável armazenar, no caso opcao, adicionando um & antes
-	
-		system("cls"); //após o usuário escolher a opção, o sistema deleta todas as mensagens anteriores
-		
-		switch(opcao) //início da seleção do menu
+		for(laco=1;laco=1;)
 		{
-			case 1: //switch case usa o : como uma chave {}
-			registro(); //chamada de funções
-			break; //break fecha as chaves do switch case, mostra quando acaba. ; porque é uma instrução
-			
-			case 2:
-			consulta();
-			break;
-			
-			case 3:
-			deletar();
-			break;
-			
-			case 4:
-			printf("Obrigado por utilizar o sistema \n\n");
-			return 0; //retorna valor 0: quebra laço, qubra função, etc.
-			break;
-			
-			default: //se for diferente do padrão, fora das opções dadas ao usuário	
-			printf("Essa opção não está disponível \n\n");
-			system("pause");
-			break;
-		}
+			system("cls"); //comando para limpar a tela
+		
+			setlocale(LC_ALL, "Portuguese"); //definir o local para conseguir usar os acentos
+
+			printf("Cartório da EBAC \n\n"); //início do menu
+			printf("Digite a opção desejada do menu: \n\n");
+			printf("\t 1 - Registrar nomes \n");
+			printf("\t 2 - Consultar nomes \n");
+			printf("\t 3 - Deletar nomes \n"); 
+			printf("\t 4 - Sair do sistema \n\n");
+			printf("Opção: ");//fim do menu
+			//\n próxima linha. \t espaçamento, margem  
 	
+			scanf ("%d", &opcao); //armazenando as informações, a escolha do usuário
+			//scanf tem como objetivo scanear o que o usuário coloca. %d armazena uma variável do tipo inteiro. indicar em qual variável armazenar, no caso opcao, adicionando um & antes
+	
+			system("cls"); //após o usuário escolher a opção, o sistema deleta todas as mensagens anteriores
+		
+			switch(opcao) //início da seleção do menu
+			{
+				case 1: //switch case usa o : como uma chave {}
+				registro(); //chamada de funções
+				break; //break fecha as chaves do switch case, mostra quando acaba. ; porque é uma instrução
+			
+				case 2:
+				consulta();
+				break;
+			
+				case 3:
+				deletar();
+				break;
+			
+				case 4:
+				printf("Obrigado por utilizar o sistema \n\n");
+				return 0; //retorna valor 0: quebra laço, qubra função, etc.
+				break;
+			
+				default: //se for diferente do padrão, fora das opções dadas ao usuário	
+				printf("Essa opção não está disponível \n\n");
+				system("pause");
+				break;
+			}
+	
+		}
 	}
+	
+	else
+		printf("\nSenha incorreta\n");
 	
 }
